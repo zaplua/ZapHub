4346958005de7109322ba90496710040e42a6e90
zaplua <kaani.bye2@gmail.com>
2024-05-14 23:04:25 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,14 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "ZapHub",
-	Text = "Loading Utils..."
-})
-loadstring(game:HttpGet('https://pastebin.com/raw/Enq9HQ29'))()
+local function sendNotification(title, text)
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = title,
+		Text = text
+	})
+end
 
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "ZapHub",
-	Text = "Loading ZapHub..."
-})
+sendNotification("ZapHub", "Loading Utils...")
+loadstring(game:HttpGet('https://egorikusa.space/78eede770504999a0cbdf477.lua'))()
+
+sendNotification("ZapHub", "Loading ZapHub...")
 loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
 
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "ZapHub",
-	Text = "Finished loading."
-})
+sendNotification("ZapHub", "Finished loading.")
